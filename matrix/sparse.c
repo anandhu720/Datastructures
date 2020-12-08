5570522a2d84a3233e6e2ff5de87c395e6452bd7
anandhu720 <anandhu4310@gmail.com>
2020-12-08 22:27:43 +0530

    dec 8

--- matrix/sparse.c
+++ matrix/sparse.c
@@ -45,10 +45,54 @@ void display(struct sparse s)
     }
 }
 
+struct sparse * add(struct sparse *s1,struct sparse *s2)
+{
+    struct sparse *sum;
+    int i=0,j=0,k=0;
+    sum=(struct sparse *)malloc(sizeof(struct sparse));
+    sum->e=(struct element *)malloc((s1->num+s2->num)*sizeof(struct element));
+    while(i<s1->num && j<s2->num)
+    {
+        if(s1->e[i].i < s2->e[j].i)
+        {
+            sum->e[k++]=s1->e[i++];
+        }
+        else if(s1->e[i].i > s2->e[j].i)
+        {
+            sum->e[k++]=s2->e[j++];
+        }
+        else 
+        {
+        if(s1->e[i].j < s2->e[j].j)
+        {
+            sum->e[k++]=s1->e[i++];
+        }
+        else if(s1->e[i].j > s2->e[j].j)
+        {
+            sum->e[k++]=s2->e[j++];
+        }
+        else 
+        {
+            sum->e[k]=s1->e[i];
+            sum->e[k++].x=s1->e[i++].x+s2->e[j++].x;
+        }
+        }
+    }
+    for(;i<s1->num;i++)
+        sum->e[k++]=s1->e[i];
+    for(;j<s2->num;j++)
+        sum->e[k++]=s2->e[j];
+    sum->m=s1->m;
+    sum->n=s1->n;
+    sum->num=k;
+}
+
 int main()
 {
-    struct sparse s;
-    create(&s);
-    display(s);
+    struct sparse s1,s2,*s3;
+    create(&s1);
+    create(&s2);
+    s3=add(&s1,&s2);
+    display(*s3);
     return 0;
 }

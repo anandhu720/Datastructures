0b9adcd1ef6f6a19d422f65efbbce23c8fba9717
anandhu720 <anandhu4310@gmail.com>
2020-12-15 22:59:56 +0530

    dec 15

--- linked list/doubly.c
+++ linked list/doubly.c
@@ -50,13 +50,55 @@ int node_count(struct node *p)
     return length;
 }
 
+//insert function in doubly linked list
+void insert(struct node *p, int index, int x)
+{
+    struct node *t;
+    t = (struct node *)malloc(sizeof(struct node));
+    if (index < 0 || index > node_count(head))
+        return;
+    if (index == 0)
+    {
+        t->data = x;
+        t->next = head;
+        t->prev = NULL;
+        head->prev = t;
+        head = t;
+    }
+    else
+    {
+        t->data = x;
+        for (int i = 0; i < index - 1; i++)
+            p = p->next;
+        t->next = p->next;
+        t->prev = p;
+        if (p->next != NULL)
+            p->next->prev = t;
+        p->next = t;
+    }
+}
+
+//max elelment doubly linked list
+int max(struct node *p)
+{
+    int x = -65536;
+    while (p != NULL)
+    {
+        if (p->data > x)
+            x = p->data;
+        p = p->next;
+    }
+    return x;
+}
+
 //main function
 int main()
 {
     int a[] = {1, 2, 3, 4, 5};
     create(a, 5);
+    insert(head, 2, 32);
 
-    printf("the lenght is %d\n", node_count(head));
+    //printf("the lenght is %d\n", node_count(head));
 
     display(head);
     printf("\n");

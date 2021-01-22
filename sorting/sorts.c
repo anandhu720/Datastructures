dbd2ff3b6bea92487ac7f2b887f7839ae9e16ebc
anandhu720 <anandhu4310@gmail.com>
2021-01-22 11:07:55 +0530

    jan 22

--- sorting/sorts.c
+++ sorting/sorts.c
@@ -100,12 +100,57 @@ void quicksort(int a[], int l, int h)
     }
 }
 
+//mergeing single array with two parts
+void merge(int a[], int l, int mid, int h)
+{
+    int i, j, k;
+    i = l;
+    j = mid + 1;
+    k = l;
+    int b[100];
+
+    while (i <= mid && j <= h)
+    {
+        if (a[i] < a[j])
+            b[k++] = a[i++];
+        else
+            b[k++] = a[j++];
+    }
+
+    //for remaining elements
+    for (; i <= mid; i++)
+        b[k++] = a[i];
+    for (; j <= h; j++)
+        b[k++] = a[j];
+
+    for (int i = 0; i <= h; i++) //changing sorted array back to A
+        a[i] = b[i];
+}
+
+//mergesort
+int mergesort(int a[], int n)
+{
+    int p, i, l, mid, h;
+    for (p = 2; p <= n; p = p * 2)
+    {
+        for (i = 0; i + p - 1 < n; i = i + p)
+        {
+            l = i;
+            h = i + p - 1;
+            mid = ((l + h) / 2);
+            merge(a, l, mid, h);
+        }
+    }
+    if (p / 2 < n)
+        merge(a, 0, p / 2 - 1, n - 1);
+}
+
 int main()
 {
     int a[] = {3, 7, 9, 10, 6, 5, 12, 4, 11, 2};
     int n = 10, i;
 
-    quicksort(a, 0, n);
+    mergesort(a, n);
 
     //printing array
     for (int i = 0; i < n; i++)

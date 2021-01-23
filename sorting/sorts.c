89c6c7b22419cd27c7a0985cbf41d38f6eb37904
anandhu720 <anandhu4310@gmail.com>
2021-01-23 11:26:19 +0530

    jan 23

--- sorting/sorts.c
+++ sorting/sorts.c
@@ -194,12 +194,32 @@ void countsort(int a[], int n)
     }
 }
 
+//shellsort
+void shellsort(int a[], int n)
+{
+    int gap, i, j, temp;
+    for (gap = n / 2; gap >= 1; gap /= 2)
+    {
+        for (i = gap; i < n; i++)
+        {
+            temp = a[i];
+            j = i - gap;
+            while (j >= 0 && a[j] > temp)
+            {
+                a[j + gap] = a[j];
+                j = j - gap;
+            }
+            a[j + gap] = temp;
+        }
+    }
+}
+
 int main()
 {
     int a[] = {3, 7, 9, 10, 6, 5, 12, 4, 11, 2};
     int n = 10, i;
 
-    countsort(a, n);
+    shellsort(a, n);
 
     //printing array
     for (int i = 0; i < n; i++)

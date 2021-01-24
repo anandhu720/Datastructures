5ba5ebc8479aff79dd92ae4ae8c69b14904ac10c
anandhu720 <anandhu4310@gmail.com>
2021-01-24 19:27:01 +0530

    jan 24

--- hashing/chaining.c
+++ hashing/chaining.c
@@ -61,7 +61,7 @@ void insert(int key)
     }
 }
 
-//searching an element in hashtable
+//searching an element in hashtable function
 int search(int key)
 {
     int hidx = hash(key);
@@ -76,4 +76,24 @@ int search(int key)
         p = p->next;
     }
     return -1;
+}
+
+int main()
+{
+    int a[] = {16, 25, 12, 39, 6, 122, 5, 68, 75};
+    int n = sizeof(a) / sizeof(a[0]);
+    for (int i = 0; i < n; i++)
+    {
+        insert(a[i]);
+    }
+
+    printf("Successfully search\n");
+    int key = 6;
+    int value = search(key);
+    printf("Key:%d , value:%d \n", key, value);
+    key = 95;
+    value = search(key);
+    printf("Key:%d , value:%d \n", key, value);
+
+    return 0;
 }

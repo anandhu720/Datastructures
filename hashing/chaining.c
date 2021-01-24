cccae61edacc888b93e27727de041c9d7aa01bf5
anandhu720 <anandhu4310@gmail.com>
2021-01-24 11:36:54 +0530

    jan 24

---
+++ hashing/chaining.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+struct node **ht;
+
+//creating hash table
+void hashtable()
+{
+    ht = (struct node **)malloc(10 * sizeof(struct node *));
+    for (int i = 0; i < 10; i++)
+        ht[i] = NULL;
+}
+
+//hashing key
+int hash(int key)
+{
+    return key % 10;
+}
+
+//inserting to hash Table
+void insert(int key)
+{
+    int hidx = hash(key); //finding hash table index
+    struct node *t;
+    t = (struct node *)malloc(sizeof(struct node));
+    t->data = key;
+    t->next = NULL;
+
+    //case 1:NO nodes in the linked list
+    if (ht[hidx] == NULL)
+    {
+        ht[hidx] = t;
+    }
+    else
+    {
+        struct node *p = ht[hidx];
+        struct node *q = ht[hidx];
+        //traverse to find inset position
+    }
+}

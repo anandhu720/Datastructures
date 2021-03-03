10568da9e18a45ac582a904e78cc8b3c73dc471b
anandhu720 <anandhu4310@gmail.com>
2021-03-03 14:02:18 +0530

    mar 3

---
+++ STL/map.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main()
+{
+    //intializing map
+    map<int, string> m;
+
+    //inserting to map
+    m.insert(pair<int, string>(1, "anandhu"));
+    m.insert(pair<int, string>(2, "raghavan"));
+    m.insert(pair<int, string>(3, "lekha"));
+    m.insert(pair<int, string>(4, "athira"));
+    m.insert(pair<int, string>(5, "loomi"));
+
+    //iterating through map
+    map<int, string>::iterator itr;
+    for (itr = m.begin(); itr != m.end(); itr++)
+    {
+        cout << itr->first << " " << itr->second << endl;
+    }
+
+    cout << "value found" << endl;
+    map<int, string>::iterator itr1;
+    itr1 = m.find(3);
+    cout << itr1->first << " " << itr1->second << endl;
+
+    cout << endl;
+    return 0;
+}
